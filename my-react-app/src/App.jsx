2edf8d74407e31242e3005c6ce79440dfac6d171
aarymalik <malikaary@gmail.com>
2025-08-08 12:11:05 +0530

    List updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,8 +2,23 @@ import Header from "./header.jsx";
 import Student from "./student.jsx";
 import Divider from "./divider.jsx";
 import UserGreetings from "./UserGreetings.jsx";
-import List from "./List.jsx";
+import List from "./Mylist.jsx";
 function App() {
+  const fruits = [
+    { id: 1, name: "Apple", calories: 95 },
+    { id: 2, name: "Orange", calories: 62 },
+    { id: 3, name: "Banana", calories: 105 },
+    { id: 4, name: "Coconut", calories: 283 },
+    { id: 5, name: "Pineapple", calories: 50 },
+  ];
+  const vegetables = [
+    { id: 6, name: "Potatos", calories: 77 },
+    { id: 7, name: "Celery", calories: 16 },
+    { id: 8, name: "Carrots", calories: 41 },
+    { id: 9, name: "Corn", calories: 86 },
+    { id: 10, name: "Brocolli", calories: 55 },
+  ];
+
   return (
     <>
       <Header />
@@ -13,7 +28,10 @@ function App() {
       <Divider />
       <UserGreetings isLoggedin={true} username="Malik Arif ul Islam" />
       <Divider />
-      <List />
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {vegetables.length > 0 && (
+        <List items={vegetables} category="Vegetables" />
+      )}
     </>
   );
 }
--- my-react-app/src/Mylist.jsx
+++ my-react-app/src/Mylist.jsx
@@ -1,12 +1,42 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 95 },
-    { id: 3, name: "banana", calories: 95 },
-    { id: 4, name: "coconut", calories: 95 },
-    { id: 5, name: "pineapple", calories: 95 },
-  ];
-  const fruitItems = fruits.map((fruit) => <li>{fruit.name}</li>);
-  return <ol> {fruitItems}</ol>;
+import PropTypes from "prop-types";
+
+function List(props) {
+  const category = props.category;
+  const itemList = props.items;
+  // Sort fruits by name in ascending order
+  // fruits.sort((a, b) => a.name.localeCompare(b.name)); //Alphabetical order
+  // fruits.sort((a, b) => b.name.localeCompare(a.name));  // Reverse alphabetical order
+  // fruits.sort((a, b) => a.calories - b.calories); // Numeric order
+  // fruits.sort((a, b) => b.calories - a.calories); // Reverse numeric order
+
+  // const lowCalorieFruits = fruits.filter((fruit) => fruit.calories < 100); // Filter fruits with less than 100 calories
+  // const highCalorieFruits = fruits.filter((fruit) => fruit.calories < 100);
+  const fruitItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name} : &nbsp;<b>{item.calories}</b>
+    </li>
+  ));
+  return (
+    <>
+      <h3>{category}</h3>
+      <ol> {fruitItems}</ol>
+    </>
+  );
 }
+
+List.propTypes = {
+  category: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      calories: PropTypes.number,
+    })
+  ),
+};
+List.defaultProps = {
+  category: "Category",
+  items: [],
+};
+
 export default List;
